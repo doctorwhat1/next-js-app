2ba8d8bcfde64729d10f3029f199b1e8f7cf9cce
doctorwhat1 <grishann007@gmail.com>
2024-10-20 18:40:32 +0300

    vercel_hook_fix3

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -22,46 +22,8 @@ import Dropdown from "@/components/dropdown";
 
 import { Person } from "@/models/Person";
 import PeopleTable from "@/components/PeopleTable";
+import { people } from "../data/people";
 
-
-export const people: Person[] = [
-  {
-    id: 1,
-    name: "Hart Hagerty",
-    country: "United States",
-    job: "Zemlak, Daniel and Leannon",
-    jobTitle: "Desktop Support Technician",
-    favoriteColor: "Purple",
-    avatar: "https://img.daisyui.com/images/profile/demo/2@94.webp",
-  },
-  {
-    id: 2,
-    name: "Brice Swyre",
-    country: "China",
-    job: "Carroll Group",
-    jobTitle: "Tax Accountant",
-    favoriteColor: "Red",
-    avatar: "https://img.daisyui.com/images/profile/demo/3@94.webp",
-  },
-  {
-    id: 3,
-    name: "Marjy Ferencz",
-    country: "Russia",
-    job: "Rowe-Schoen",
-    jobTitle: "Office Assistant I",
-    favoriteColor: "Crimson",
-    avatar: "https://img.daisyui.com/images/profile/demo/4@94.webp",
-  },
-  {
-    id: 4,
-    name: "Yancy Tear",
-    country: "Brazil",
-    job: "Wyman-Ledner",
-    jobTitle: "Community Outreach Specialist",
-    favoriteColor: "Indigo",
-    avatar: "https://img.daisyui.com/images/profile/demo/5@94.webp",
-  },
-];
 
 
 const items = [
